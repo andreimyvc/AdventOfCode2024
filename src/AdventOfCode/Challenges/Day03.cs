e91879b21eb96409f0c5acfe0107ec1f17a27266
CVilorio <cvilorio@outlook.com>
2024-12-11 00:39:23 -0400

    Dia 03 parte 01

---
+++ src/AdventOfCode/Challenges/Day03.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+
+namespace Day01.Challenges;
+public partial class Challege
+{
+    public static void Day03()
+    {
+        var data = File.ReadAllText("inputs/day03.txt");
+
+        Console.WriteLine($"Parte 1: {Part1(data)}");
+        Console.ReadLine();
+
+        int Part1(string data)
+        {
+            var lista = Parser(data);
+            return lista.Select(p => p.Split(',')).Select(p => int.Parse(p[0]) * int.Parse(p[1])).Sum();            
+        }
+
+        List<string> Parser(string data)
+        {
+            List<string> result = new();
+            string temp = string.Empty;
+            bool flag = false;
+
+            int i = 0;
+
+            while(i < data.Length)
+            {
+
+                if ((i + 3) < data.Length && $"{data[i]}{data[i + 1]}{data[i + 2]}{data[i + 3]}" == "mul(")
+                {
+                    flag = true;
+                    
+                    i += 4;
+
+                    temp = string.Empty;
+                }
+
+
+                char c = data[i];
+
+                if (flag)
+                {
+
+                    if (char.IsDigit(c) || c == ',')
+                    {
+                        temp += c;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+
+                if (c == ')' || temp.Length > 7)
+                {
+                    if (!string.IsNullOrWhiteSpace(temp) && temp.Length <= 7 && temp.Contains(','))
+                    {
+                        result.Add(temp);
+                    }
+                }
+                
+                if (!flag)
+                {
+                    temp = string.Empty;
+                }
+
+                i++;
+            }
+
+            return result;
+        }
+        
+    }
+}
+
+/*
+ 
+ what(){mul(697,542)/>why(479,94)mul(995,893)why()]%?:]select()%}mul(408,907),{from()$&&/mul(893,282)[#%@(who()what()!~mul(313,566)
+ 
+ */
--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -4,5 +4,7 @@ using Day01.Challenges;
 
 //Challege.Day01();
 
-Challege.Day02();
+//Challege.Day02();
+
+Challege.Day03();
 
