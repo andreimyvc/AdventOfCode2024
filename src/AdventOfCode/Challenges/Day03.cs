f2134da7aee425ee9bd8bc262acb189bf86be8ec
CVilorio <cvilorio@outlook.com>
2024-12-19 02:07:51 -0400

    Dia 03 parte 02

--- src/AdventOfCode/Challenges/Day03.cs
+++ src/AdventOfCode/Challenges/Day03.cs
@@ -8,12 +8,21 @@ public partial class Challege
         var data = File.ReadAllText("inputs/day03.txt");
 
         Console.WriteLine($"Parte 1: {Part1(data)}");
+
+        Console.WriteLine($"Parte 2: {Part2(data)}");
         Console.ReadLine();
+
 
         int Part1(string data)
         {
             var lista = Parser(data);
             return lista.Select(p => p.Split(',')).Select(p => int.Parse(p[0]) * int.Parse(p[1])).Sum();            
+        }
+
+        int Part2(string data)
+        {
+            var lista = Parser2(data);
+            return lista.Select(p => p.Split(',')).Select(p => int.Parse(p[0]) * int.Parse(p[1])).Sum();
         }
 
         List<string> Parser(string data)
@@ -70,12 +79,67 @@ public partial class Challege
 
             return result;
         }
-        
+
+        List<string> Parser2(string data)
+        {
+            List<string> result = new();
+            string temp = string.Empty;
+            bool flag = false;
+            bool enable = true;
+            int i = 0;
+
+            while (i < data.Length)
+            {
+
+                if ((i + 4) < data.Length && data.Substring(i, 4) == "mul(")
+                {
+                    flag = true;
+
+                    i += 4;
+
+                    temp = string.Empty;
+                }
+
+                if ((i + 7) < data.Length && data.Substring(i, 7) == "don't()")
+                {
+                    enable = false;
+                    i += 7;
+                }
+
+                if ((i + 4) < data.Length && data.Substring(i, 4) == "do()")
+                {
+                    enable = true;
+                    i += 4;
+                    continue;
+                }
+
+                char c = data[i];
+
+                if (flag)
+                {
+                    if (enable && c == ')' && temp.Length <= 7 && temp.Contains(','))
+                    {
+                        result.Add(temp);
+                    }
+
+                    if (char.IsDigit(c) || c == ',')
+                    {
+                        temp += c;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                } 
+                else
+                {
+                    temp = string.Empty;
+                }
+
+                i++;
+            }
+
+            return result;
+        }
     }
 }
-
-/*
- 
- what(){mul(697,542)/>why(479,94)mul(995,893)why()]%?:]select()%}mul(408,907),{from()$&&/mul(893,282)[#%@(who()what()!~mul(313,566)
- 
- */
