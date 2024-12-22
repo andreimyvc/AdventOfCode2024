0ee2e5ce44e63518baf8c29908b71382aa099ea6
CVilorio <cvilorio@outlook.com>
2024-12-21 22:02:50 -0400

    Dia 04 parte 02

--- src/AdventOfCode/Challenges/Day04.cs
+++ src/AdventOfCode/Challenges/Day04.cs
@@ -9,6 +9,8 @@ public partial class Challege
 
         Console.WriteLine($"Parte 1: {Part1(data)}");
 
+        Console.WriteLine($"Parte 2: {Part2(data)}");
+
         Console.ReadLine();
 
 
@@ -70,6 +72,41 @@ public partial class Challege
 
                         //Diagonal Down Derecha
                         if (y + 3 < data.Length && x + 3 < line.Length && $"{line[x]}{data[y + 1][x + 1]}{data[y + 2][x + 2]}{data[y + 3][x + 3]}" == "XMAS")
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        int Part2(string[] data)
+        {
+            int count = 0;
+
+            for (int y = 0; y < data.Length; y++)
+            {
+                string line = data[y];
+
+                for (int x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == 'A' && y - 1 >= 0 && x - 1 >= 0 && y + 1 < data.Length && x + 1 < line.Length)
+                    {
+                        if (data[y - 1][x - 1] == 'S' && data[y - 1][x + 1] == 'S' && data[y + 1][x - 1] == 'M' && data[y + 1][x + 1] == 'M')
+                        {
+                            count++;
+                        }
+                        else if (data[y - 1][x - 1] == 'M' && data[y - 1][x + 1] == 'M' && data[y + 1][x - 1] == 'S' && data[y + 1][x + 1] == 'S')
+                        {
+                            count++;
+                        }
+                        else if (data[y - 1][x - 1] == 'S' && data[y - 1][x + 1] == 'M' && data[y + 1][x - 1] == 'S' && data[y + 1][x + 1] == 'M')
+                        {
+                            count++;
+                        }
+                        else if (data[y - 1][x - 1] == 'M' && data[y - 1][x + 1] == 'S' && data[y + 1][x - 1] == 'M' && data[y + 1][x + 1] == 'S')
                         {
                             count++;
                         }
